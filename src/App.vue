e389d48929e2ce8c64f1731dbe358d28bdc1cb7a
Ner08 <robic.nejc1122@gmail.com>
2023-10-27 02:16:08 +0200

    side bar implemented

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue'
+</script>
+
+<template>
+  <SideBar>
+    <nav-link class="flex-shrink-1">
+      <h1>to be continued...</h1>
+    </nav-link>
+
+  </SideBar>
+</template>
+
+<style scoped></style>

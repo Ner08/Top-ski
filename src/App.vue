e4b3d77fa6e0fef3d47d08f71279aa3054ef7d0f
Ner08 <robic.nejc1122@gmail.com>
2023-10-27 19:56:27 +0200

    organized the app into components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import SideBar from './components/SideBar.vue'
+import SideBar from './components/sidebar-components/SideBar.vue'
 </script>
 
 <template>
   <SideBar>
-    <nav-link class="flex-shrink-1">
-      <h1>to be continued...</h1>
-    </nav-link>
-
+    <router-view class="flex-shrink-1"></router-view>
   </SideBar>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,14 @@
 </script>
 
 <template>
-  <main>
-  </main>
+  <div class="col col-md-6 col-sm-12 col-12 border-right border-light bg-black">
+    <ul class="nav nav-underline d-flex align-items-center justify-content-center ">
+      <li class="nav-item btn">
+        <a class="nav-link active  mx- 0 mx-md-5" aria-current="page" href="#">For you</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Following</a>
+      </li>
+    </ul>
+  </div>
 </template>

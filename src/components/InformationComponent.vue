ddf411c85ce0bcd85347c8ef987c2431116a5ee3
Nejc Robič <nejc.robic@pronet-kr.si>
2024-01-09 16:52:02 +0100

    changed some text

--- src/components/InformationComponent.vue
+++ src/components/InformationComponent.vue
@@ -19,7 +19,7 @@
                 </div>
                 <!-- TODO: Change location -->
                 <div class="col-md">
-                    <iframe class="container rounded-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2747.0946105047797!2d13.833693856093136!3d46.4864389968439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477a7d79d9cb4d91%3A0x489a0308c0013708!2sJezerci%2023b%2C%204282%20Gozd%20Martuljek!5e0!3m2!1ssl!2ssi!4v1701881390231!5m2!1ssl!2ssi" width="600" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <!-- <iframe class="container rounded-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2747.0946105047797!2d13.833693856093136!3d46.4864389968439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477a7d79d9cb4d91%3A0x489a0308c0013708!2sJezerci%2023b%2C%204282%20Gozd%20Martuljek!5e0!3m2!1ssl!2ssi!4v1701881390231!5m2!1ssl!2ssi" width="600" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
                 </div>
             </div>
         </div>
